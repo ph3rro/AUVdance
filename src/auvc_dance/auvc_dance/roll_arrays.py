574ee3a81eb5b4dab76e4d401c87353f0bf99f47
Yujie Jin <arceascompany@gmail.com>
2026-07-16 16:31:48 -0400

    Added unfinished roll_arrays file.

---
+++ src/auvc_dance/auvc_dance/roll_arrays.py
@@ -0,0 +1,12 @@
+import time
+import numpy as np
+
+class Claps:
+    def hop_once():
+        movement_arr = [0.0, 0.0, 0.75, 0.0, 0.0, 0.0]
+        return movement_arr
+    
+    def stop():
+        return [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    
+    
