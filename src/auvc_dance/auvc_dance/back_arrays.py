0fdbd266ebc590388fe9b59619a30abbbdbcaddb
Caitlin C <cai.caitlin@gmail.com>
2026-07-16 16:20:11 -0400

    back arrays

--- src/auvc_dance/auvc_dance/back_arrays.py
+++ src/auvc_dance/auvc_dance/back_arrays.py
@@ -4,9 +4,9 @@ import rclpy    # the ROS 2 client library for Python
 from rclpy.node import Node    # the ROS 2 Node class
 import numpy as np
 
-class Hops:
-    def hop_once():
-        movement_arr = [0.0, 0.0, 0.5, 0.0, 0.0, 0.0]
+class back_arrays:
+    def move_back():
+        movement_arr = [0.0, -0.5, 0.0, 0.0, 0.0, 0.0]
         return movement_arr
     
     def stop():
