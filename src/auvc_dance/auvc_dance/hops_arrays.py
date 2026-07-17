7c3438b0945f0cc8a7ead88ee9525b939bf608ed
Yujie Jin <arceascompany@gmail.com>
2026-07-16 20:45:08 -0400

    Finalized Hops class (hopefully).

--- src/auvc_dance/auvc_dance/hops_arrays.py
+++ src/auvc_dance/auvc_dance/hops_arrays.py
@@ -1,9 +1,8 @@
-import time
-import numpy as np
-
 class Hops:
-    def hop(secs, strength):
+    '''Hops.hop outputs and array of format [duration, x_thrust, y_thrust, z_thrust, angular_thrust]
+       secs: Duration of movement in seconds
+       strength: strength of thrust out of 500.0
+       spin: strangth of rotation out of 500'''
+    def hop(secs, strength, spin = 0):
         neutral = 500.0
-        return [secs, neutral, neutral, neutral + strength, neutral]
-    
-    
+        return [secs, neutral, neutral, neutral + strength, neutral + spin]
